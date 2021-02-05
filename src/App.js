fb37bc590d1b11e3ae0b2e0117b9d89884248f99
Héctor Berumen <hberumen@uaz.edu.mx>
2021-02-05 13:54:54 -0600

    aplicacion terminada

--- src/App.js
+++ src/App.js
@@ -1,24 +1,78 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment, useEffect, useState } from 'react' 
+import Formulario from './components/Formulario';
+import Clima from './components/Clima';
+import Header from "./components/Header";
+import Error from "./components/Error";
+
 
 function App() {
+
+  const [busqueda, guardarBusqueda] = useState({
+    ciudad: '',
+    pais: ''
+  })
+
+  const [consultar, guardarConsultar] = useState(false)
+  const [resultado, guardarResultado] = useState({})
+  const [error, guardarError] = useState(false)
+  const { ciudad, pais } = busqueda
+
+
+  useEffect(() => {
+    const consultarAPI = async () => {
+
+      if(consultarAPI){
+        const appId = '0394d75f47e1b1cd306a509d5b88e427';
+        const url = `https://api.openweathermap.org/data/2.5/weather?q=${ciudad},${pais}&appid=${appId}`;
+
+        const respuesta = await fetch(url)
+        const resultado = await respuesta.json()
+
+        guardarResultado(resultado)
+        guardarConsultar(false)
+
+        if(resultado.cod === "404"){
+          guardarError(true)
+        }else{
+          guardarError(false)
+        }
+      }
+
+    }
+    consultarAPI()
+  //eslint-disable-next-line
+  },[consultar])
+
+  let componente;
+  if(error){
+    componente = <Error mensaje="No hay Resultados" />
+  }else{
+    componente = <Clima resultado={resultado} />
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+        <Fragment>
+          <Header
+            titulo="Clima react APP"
+          />
+          <div className="contenedor-form">
+              <div className="container">
+                  <div className="row">
+                    <div className="col m6 s12">
+                      <Formulario 
+                          busqueda={busqueda}
+                          guardarBusqueda={guardarBusqueda}
+                          guardarConsultar={guardarConsultar}
+                      />
+                    </div>
+                    <div className="col m6 s12">
+                        {componente}
+                    </div>
+                  </div>
+              </div>
+          </div>
+        </Fragment>
+        
   );
 }
 
